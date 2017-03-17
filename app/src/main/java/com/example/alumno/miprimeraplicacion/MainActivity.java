7f387e27655aaf6a1e86da251fea34505d4ec40b
alumno <alumno>
2017-03-16 21:54:25 -0300

    Primer Commit

---
+++ app/src/main/java/com/example/alumno/miprimeraplicacion/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.alumno.miprimeraplicacion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView tv = (TextView) findViewById(R.id.tv_Saludo);
+        tv.setText("jojo");
+
+    }
+}
